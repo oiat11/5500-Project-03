07f6f3fa394a663beb442c2142c7cc0cac5a540e
oiat11 <fu.qia@northeastern.edu>
2025-03-02 18:24:31 -0800

    initial set up for the front end

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+       <Button>Button</Button>
+    </>
+  )
+}
+
+export default App
