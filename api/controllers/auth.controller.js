4002c59776e3e5df688842c1c3806a3ec8fe526d
oiat11 <fu.qia@northeastern.edu>
2025-03-04 16:42:40 -0800

    add user log in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
+import {errorHandler} from "../utils/error.js";
 
 const prisma = new PrismaClient();
 
@@ -19,3 +21,27 @@ export const signUp = async (req, res, next) => {
     }
 };
 
+export const logIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!validUser) return next(errorHandler(404, "User not found"));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+
+        const token = jwt.sign({ id: validUser.id }, process.env.JWT_SECRET, {
+            expiresIn: "7d", 
+        });
+
+        const { password: pass, ...rest } = validUser;
+
+        res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {signUp} from '../controllers/auth.controller.js';
+import {signUp, logIn} from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/signup', signUp);
+router.post('/login', logIn)
 
 export default router;
