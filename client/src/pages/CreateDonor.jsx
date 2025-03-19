a4809a196730b3b9bba140bc2028fb93a0f2f3e3
oiat11 <fu.qia@northeastern.edu>
2025-03-18 17:10:50 -0700

    add a createDonor page and add the create donor button in Donors page

---
+++ client/src/pages/CreateDonor.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CreateDonor() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Create New Donor</h1>
+      <p>Add a new donor form will go here.</p>
+    </div>
+  )
+}
