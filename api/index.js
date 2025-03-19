572b93638d2f57202ec0449066e1c2f46f79ef8e
oiat11 <fu.qia@northeastern.edu>
2025-03-19 09:50:56 -0700

    add UI and functionality to create donor page

--- api/index.js
+++ api/index.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';  // ✅ 引入 CORS
 import authRoutes from './routes/auth.route.js';
 import donorRoutes from './routes/donor.route.js';
-
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
+
+
+app.use(cors({
+    origin: "http://localhost:5173", 
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    credentials: true,  
+}));
 
 const PORT = process.env.PORT || 3000;
 
@@ -27,8 +33,9 @@ app.use('/api/donor', donorRoutes);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({ success: false,
+    return res.status(statusCode).json({
+        success: false,
         statusCode,
         message,
-     });
+    });
 });
